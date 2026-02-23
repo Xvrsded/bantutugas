19bae9f4bd61810960cdf12510f5846339dc6c59
Muhamad Guntur Irwansyah <gunturirwansyahh@gmail.com>
2026-02-23 12:52:17 +0700

    Ensure view binding exists in production boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\View\ViewServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (! $this->app->bound('view')) {
+            $this->app->register(ViewServiceProvider::class);
+        }
     }
 
     /**
